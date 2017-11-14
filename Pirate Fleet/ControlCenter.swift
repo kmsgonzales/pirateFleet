42b0dbef162158b3c5de4d0514678f3d7bfdd714
Katherine-Marie Gonzales <kmsgonzales@comcast.net>
2017-11-13 21:34:06 -0800

    Added ships and mines

--- Pirate Fleet/ControlCenter.swift
+++ Pirate Fleet/ControlCenter.swift
@@ -17,10 +17,28 @@ struct Ship {
     let isVertical: Bool
 }
 
+struct Mine: _Mine_ {
+    let location: GridLocation
+    let explosionText: String
+}
+
 class ControlCenter {
     
     func addShipsAndMines(_ human: Human) {
-        // Write your code here!
+        let mediumShip1 = Ship(length: 3, location: GridLocation(x: 2, y:3),isVertical: true)
+        human.addShipToGrid(mediumShip1)
+        let mediumShip2 = Ship(length: 3, location: GridLocation(x: 6, y:2), isVertical: true)
+        human.addShipToGrid(mediumShip2)
+        let smallShip = Ship(length: 2, location: GridLocation(x: 4, y:3), isVertical: true)
+        human.addShipToGrid(smallShip)
+        let largeShip = Ship(length: 4, location: GridLocation(x: 2, y: 7), isVertical: false)
+        human.addShipToGrid(largeShip)
+        let xlShip = Ship(length: 5, location: GridLocation(x:1, y: 1), isVertical: false)
+        human.addShipToGrid(xlShip)
+        let mine1 = Mine(location: GridLocation(x: 2, y:2), explosionText: "Oh, no! You've hit a mine! The opponent gets another turn!")
+        human.addMineToGrid(mine1)
+        let mine2 = Mine(location: GridLocation(x:6, y: 6), explosionText: "A mine! Your opponent has beeen rewarded an extra turn!")
+        human.addMineToGrid(mine2)
     }
     
     func calculateFinalScore(_ gameStats: GameStats) -> Int {
